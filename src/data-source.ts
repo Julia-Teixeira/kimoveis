4bbb152d80a537c045001ae49b73974aa4537f42
Julia Pereira <juliapt95@gmail.com>
2023-03-18 21:35:26 -0300

    fix: error metadata

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,8 @@ import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts, js}");
-  const migrationPath: string = path.join(
-    __dirname,
-    "./migrations/**.{ts, js}"
-  );
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationPath: string = path.join(__dirname, "./migrations/**.{ts,js}");
 
   const dbURL: string | undefined = process.env.DATABASE_URL;
 
